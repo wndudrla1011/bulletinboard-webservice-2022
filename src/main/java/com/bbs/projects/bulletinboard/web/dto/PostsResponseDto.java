622c35f73c3be3d453a2692831263bf4b7740cbd
wndudrla1011 <wndudrla1011@naver.com>
2022-07-06 21:16:37 +0900

    completed JPA code

---
+++ src/main/java/com/bbs/projects/bulletinboard/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.bbs.projects.bulletinboard.web.dto;
+
+import com.bbs.projects.bulletinboard.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
