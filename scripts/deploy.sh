11cd63fd690935df348400925cda01b08e8209b4
wndudrla1011 <wndudrla1011@naver.com>
2022-07-23 11:10:32 +0900

    test4 releasing 1.0.3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,11 +14,11 @@ CURRENT_PID=$(pgrep -fl bulletinboard-webservice-2022 | grep jar | awk '{print $
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않는다."
+  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않는다."
 else
-	 echo "> kill -9 $CURRENT_PID"
-   kill -9 $CURRENT_PID
-   sleep 5
+  echo "> kill -9 $CURRENT_PID"
+  kill -9 $CURRENT_PID
+  sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
