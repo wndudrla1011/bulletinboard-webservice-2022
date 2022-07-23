60ef99573a71ffbdc8bcc8cc44d78c1bad03c6c7
wndudrla1011 <wndudrla1011@naver.com>
2022-07-23 21:12:11 +0900

    added ProfileControllerUnitTest

--- src/test/java/com/bbs/projects/bulletinboard/web/ProfileControllerTest.java
+++ src/test/java/com/bbs/projects/bulletinboard/web/ProfileControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mock.env.MockEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.*;
@@ -23,73 +22,18 @@ public class ProfileControllerTest {
     //필드 주입
     @Autowired
     private TestRestTemplate restTemplate;
-
-    @Test
-    public void read_real_profile() {
-
-        //given
-        String expectedProfile = "real";
-        MockEnvironment env = new MockEnvironment(); //Environment 가짜 구현체
-        //profile 추가
-        env.addActiveProfile(expectedProfile);
-        env.addActiveProfile("oauth");
-        env.addActiveProfile("real-db");
-
-        ProfileController controller = new ProfileController(env);
-
-        //when
-        String profile = controller.profile(); //필터링된 profile 추출
-
-        //then
-        assertThat(profile).isEqualTo(expectedProfile);
-
-    }
-
-    @Test
-    public void read_first_if_not_real_profile() {
-        //given
-        String expectedProfile = "oauth";
-        MockEnvironment env = new MockEnvironment(); //Environment 가짜 구현체
-
-        //profile 추가
-        env.addActiveProfile(expectedProfile);
-        env.addActiveProfile("real-db");
-
-        ProfileController controller = new ProfileController(env);
-
-        //when
-        String profile = controller.profile(); //필터링된 profile 추출
-
-        //then
-        assertThat(profile).isEqualTo(expectedProfile);
-    }
-
-    @Test
-    public void read_default_if_not_active_profile() {
-        //given
-        String expectedProfile = "default";
-        MockEnvironment env = new MockEnvironment(); //Environment 가짜 구현체
-        ProfileController controller = new ProfileController(env);
-
-        //when
-        String profile = controller.profile(); //필터링된 profile 추출
-
-        //then
-        assertThat(profile).isEqualTo(expectedProfile);
-
-    }
     
-//    @Test
-//    public void call_profile_without_certification() throws Exception{
-//
-//        String expected = "default";
-//        //http 헤더 접근을 위해 ResponseEntity를 받음
-//        ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
-//        //HTTP 헤더 상태 검증
-//        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-//        //HTTP 바디에 "default"가 있는지 검증
-//        assertThat(response.getBody()).isEqualTo(expected);
-//
-//    }
+    @Test
+    public void call_profile_without_certification() throws Exception{
+
+        String expected = "default";
+        //http 헤더 접근을 위해 ResponseEntity를 받음
+        ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
+        //HTTP 헤더 상태 검증
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        //HTTP 바디에 "default"가 있는지 검증
+        assertThat(response.getBody()).isEqualTo(expected);
+
+    }
 
 }
