b36a49b030c6798d5dd5b25cc9e9a58784eec980
wndudrla1011 <wndudrla1011@naver.com>
2022-07-25 15:54:03 +0900

    catch error in switch.sh

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -10,7 +10,7 @@ function switch_proxy() {
     echo "> 전환할 Port: $IDLE_PORT"
     echo "> Port 전환"
     # /etc/nginx/conf.d/service_url.inc에 앞의 문장을 덮어씀
-    echo "set /$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
     echo "> 엔진엑스 Reload"
     sudo service nginx reload # service-url.inc를 다시 불러옴
 }
